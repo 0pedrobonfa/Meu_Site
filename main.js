13571fafc971819ccb932c6393806b4fd4581708
Pedro Bonfá <pedro.bonfa@estuidante.ufjf.br>
2024-05-02 15:24:34 -0300

    add login page & page to publish reviews

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+
+
 document.getElementById("botaoReviews").addEventListener("click",function(){
     window.location.href = "paginaReview.html";
 })
+
+document.getElementById("btnSend").addEventListener("click",function ()
+{
+    var login = document.getElementById('login');
+    var senha = document.getElementById('senha');
+    
+    if (login == "admin" && senha == "admin")
+    {
+        alert('Sucesso');
+    }
+    else
+    {
+        alert('Fracasso');
+    }
+
+
+});
+
+
