66019132af2e400e59cbe7b81470f97a6af3485b
opedrobonfa <pedro.bonfa@estudante.ufjf.br>
2024-05-03 18:13:20 -0300

    nem todas as esperanças da branchLogin estão perdidas?veremos no próximo episódio

--- public.js
+++ public.js
@@ -1,28 +1,22 @@
+
 let btnSendNewReview = document.getElementById("btnSendNewReview");
 btnSendNewReview.addEventListener("click", function () {
-    let texto = document.getElementById("novaReview").value;
-    let textoConvertido = converteParaHTML(texto);
 
-    // Criar um novo documento HTML
-    let novoDocumento = document.implementation.createHTMLDocument("Novo Documento");
+   let texto = document.getElementById("novaReview").value;
+   converteParaHTML(texto);
 
-    // Adicionar o texto convertido ao corpo do novo documento
-    novoDocumento.body.innerHTML = textoConvertido;
+    function converteParaHTML(texto) {
+        let linhas = texto.split('\n')
+         let textoConvertido = '';       linhas.forEach(linha => {
+             if (linha !== '') {
+                 textoConvertido +=`<p>${linha}</p>\n`;
+           } else {
+               textoConvertido += '<p></p>\n';
+           }
+       });
+       document.getElementById('resultado').innerHTML = textoConvertido;
+   }
 
-    // Abrir o novo documento em uma nova janela
-    let novaJanela = window.open();
-    novaJanela.document.write(novoDocumento.documentElement.outerHTML);
-});
+ });
 
-function converteParaHTML(texto) {
-    let linhas = texto.split('\n')
-    let textoConvertido = '';
-    linhas.forEach(linha => {
-        if (linha !== '') {
-            textoConvertido += `<p>${linha}</p>\n`;
-        } else {
-            textoConvertido += '<p></p>\n';
-        }
-    });
-    return textoConvertido;
-}
+document.getElementById('tituloReview04').innerHTML = "Sonic 2" 
