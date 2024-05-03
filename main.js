116beea44d86f797aead2be8421030b704c067b4
Pedro Bonfá <pedro.bonfa@estuidante.ufjf.br>
2024-05-02 21:22:09 -0300

    coloca mais conteúdo no site e faz algumas alterações => v1.2

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-document.getElementById("botaoReviews").addEventListener("click",function(){
-    window.location.href = "paginaReview.html";
+document.getElementById("r01").addEventListener("click",function(){
+    window.location.href = "Review01.html";
+})
+
+document.getElementById("r02").addEventListener("click",function(){
+    window.location.href = "Review02.html";
+})
+
+document.getElementById("r03").addEventListener("click",function(){
+    window.location.href = "Review03.html";
 })
